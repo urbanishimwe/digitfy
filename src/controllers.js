23c212c4b02c5aa2fd4ca8b0ebd9f5f9024cdff8
urban ishimwe <urbainishimwe@gmail.com>
2019-06-02 11:46:25 +0200

    es6 models

--- src/controllers.js
+++ src/controllers.js
@@ -2,208 +2,100 @@ import Middlewares from './middlewares';
 import Helpers from './helpers';
 
 export default class Controllers {
+
     static getNumber(string) {
-        const numbers = Middlewares.match(string, Helpers.getNumber);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getNumber);
     }
+
     static getNegativeNumber(string) {
-        const numbers = Middlewares.match(string, Helpers.getNumberNegative);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getNumberNegative);
     }
+
     static getPositiveNumber(string) {
-        const numbers = Middlewares.match(string, Helpers.getNumberPositive);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getNumberPositive);
     }
+
     static getNumberExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getNumber);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = (element[0].endsWith('.')) ? element[0].length - 1 : element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getNumber);
     }
+
     static getNumberNegativeExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getNumberNegative);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = (element[0].endsWith('.')) ? element[0].length - 1 : element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getNumberNegative);
     }
+
     static getNumberPositiveExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getNumberPositive);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = (element[0].endsWith('.')) ? element[0].length - 1 : element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getNumberPositive);
     }
+
     static getInt(string) {
-        const numbers = Middlewares.match(string, Helpers.getInt);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getInt);
     }
+
     static getNegativeInt(string) {
-        const numbers = Middlewares.match(string, Helpers.geIntNegative);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getIntNegative);
     }
+
     static getPositiveInt(string) {
-        const numbers = Middlewares.match(string, Helpers.getIntPositive);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getIntPositive);
     }
+
     static getIntExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getInt);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getInt);
     }
+
     static getIntNegativeExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getIntNegative);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getIntNegative);
     }
+
     static getIntPositiveExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getIntPositive);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getIntPositive);
     }
+
     static getFloor(string) {
-        const numbers = Middlewares.match(string, Helpers.getFloor);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getFloor);
     }
+
     static getNegativeFloor(string) {
-        const numbers = Middlewares.match(string, Helpers.getFloorNegative);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getFloorNegative);
     }
+
     static getPositiveFloor(string) {
-        const numbers = Middlewares.match(string, Helpers.getFloorPostive);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getFloorPostive);
     }
+
     static getFloorExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getFloor);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getFloor);
     }
     static getNegativeFloorExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getFloorNegative);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getFloorNegative);
     }
+
     static getPositiveFloorExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getFloorPostive);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getFloorPostive);
     }
+
     static getExponential(string) {
-        const numbers = Middlewares.match(string, Helpers.getExponential);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getExponential);
     }
+
     static getNegativeExponential(string) {
-        const numbers = Middlewares.match(string, Helpers.getExponentialNegative);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getExponentialNegative);
     }
+
     static getPositiveExponential(string) {
-        const numbers = Middlewares.match(string, Helpers.getExponentialPositive);
-        numbers.forEach((element, index) => {
-            numbers[index] = Number(element);
-        });
-        return numbers;
+        return Middlewares.getMatch(string, Helpers.getExponentialPositive);
     }
+
     static getExponentialExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getExponential);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getExponential);
     }
+
     static getNegativeExponentialExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getExponentialNegative);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getExponentialNegative);
     }
+
     static getPositiveExponentialExec(string) {
-        const numbers = Middlewares.exec(string, Helpers.getExponentialPositive);
-        const numbersExec = [];
-        numbers.forEach((element, index) => {
-            const end = element[0].length;
-            element[0] = Number(element[0]);
-            numbersExec.push({number: element[0], start: element.index, end});
-        });
-        return numbersExec;
+        return Middlewares.getExec(string, Helpers.getExponentialPositive);
     }
+    
 }
--- src/middlewares.js
+++ src/middlewares.js
@@ -10,4 +10,21 @@ export default class Middlewares {
         }
         return searches;
     }
+    static getMatch(string, regex) {
+        const numbers = Middlewares.match(string, regex);
+        numbers.forEach((element, index) => {
+            numbers[index] = Number(element);
+        });
+        return numbers;
+    }
+    static getExec(string, regex) {
+        const numbers = Middlewares.exec(string, regex);
+        const numbersExec = [];
+        numbers.forEach((element, index) => {
+            const end = (element[0].endsWith('.')) ? element[0].length - 1 : element[0].length;
+            element[0] = Number(element[0]);
+            numbersExec.push({number: element[0], start: element.index, end});
+        });
+        return numbersExec;
+    }
 }
